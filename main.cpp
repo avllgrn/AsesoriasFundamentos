c4c7721b1cc4c4abef67d61b4bddaef672ce8f03
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-06-28 13:31:38 -0500

    Contador descendente

--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 #include <math.h>
 
 int main(void){
-    int i,ini,fin,inc;
+    int i,ini,fin,dec;
 
     printf("Ingresa ini ");scanf("%d",&ini);
     printf("Ingresa fin ");scanf("%d",&fin);
-    printf("Ingresa inc ");scanf("%d",&inc);
+    printf("Ingresa dec ");scanf("%d",&dec);
 
     i=ini;                      // Inicio
     do{
         printf("%d\n",i);
 
-        i = i+inc;              // Inc/Dec
-    }while( i <= fin );         // Final
+        i = i-dec;              // Inc/Dec
+    }while( i >= fin );         // Final
 
     //printf("Despues de ciclo %d\n",i);
 
