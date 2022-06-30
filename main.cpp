301c0c74c3331bb7ba40ce06654f57d8af430503
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-06-30 13:43:03 -0500

    Suma y resta de vectores

--- main.cpp
+++ main.cpp
@@ -3,45 +3,42 @@
 #include <time.h>
 
 int main(void){
-    float V[50];
-    int i,n;
+    int A[50], B[50], C[50], D[50];
+    int i,n1,n2;
     srand(time(NULL));
 
-    printf("Ingresa n ");scanf("%d",&n);
-
-    for(i=0;i<n;i++){
-        printf("V[%d] = %f\n",i,V[i]);
-    }
+    printf("Ingresa n1 ");
+    scanf("%d",&n1);
+    printf("Ingresa n2 ");
+    scanf("%d",&n2);
     printf("\n\n");
 
-    for(i=0;i<n;i++){
-        V[i] = 0;
+    if(n1!=n2){
+        printf("Error! No pueden sumarse ni restarse...=^(\n");
     }
+    else{
+        for(i=0; i<n1; i++)
+            A[i] = rand()%10;
 
-    for(i=0;i<n;i++){
-        printf("V[%d] = %f\n",i,V[i]);
-    }
-    printf("\n\n");
+        for(i=0; i<n2; i++)
+            B[i] = rand()%10;
 
-    for(i=0;i<n;i++){
-        printf("Ingresa V[%d] ",i);
-        scanf("%f",&V[i]);
-    }
-    printf("\n\n");
+        for(i=0; i<n1; i++)
+            C[i] = A[i] + B[i];
 
-    for(i=0;i<n;i++){
-        printf("V[%d] = %f\n",i,V[i]);
-    }
-    printf("\n\n");
+        for(i=0; i<n1; i++)
+            printf("%d + %d = %d\n",A[i],B[i],C[i]);
 
-    for(i=0;i<n;i++){
-        V[i] = rand()%10;
-    }
+        printf("\n\n");
 
-    for(i=0;i<n;i++){
-        printf("V[%d] = %f\n",i,V[i]);
+        for(i=0; i<n1; i++)
+            D[i] = A[i] - B[i];
+
+        for(i=0; i<n1; i++)
+            printf("%d - %d = %d\n",A[i],B[i],D[i]);
+
+        printf("\n\n");
     }
-    printf("\n\n");
 
     return 0;
 }
