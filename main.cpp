fb9b8e3157aad41a96268dc16d6a83aca08707f9
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-07-01 14:22:47 -0500

    Suma y resta de matrices

--- main.cpp
+++ main.cpp
@@ -3,47 +3,81 @@
 #include <time.h>
 
 int main(void){
-    int M[50][50], MT[50][50];
-    int i,j,m,n,mT,nT;
+    int A[50][50], B[50][50], C[50][50], D[50][50];
+    int i,j,m1,n1,m2,n2;
     srand(time(NULL));
 
-    printf("Ingresa filas ");scanf("%d",&m);
-    printf("Ingresa columnas ");scanf("%d",&n);
+    printf("Ingresa filas1 ");scanf("%d",&m1);
+    printf("Ingresa columnas1 ");scanf("%d",&n1);
 
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            M[i][j] = rand()%10;
+    printf("Ingresa filas2 ");scanf("%d",&m2);
+    printf("Ingresa columnas2 ");scanf("%d",&n2);
+
+    if(m1==m2 && n1==n2){
+
+        for(i=0;i<m1;i++){
+            for(j=0;j<n1;j++){
+                A[i][j] = rand()%10;
+            }
         }
-    }
 
-
-    //Matriz Transpuesta
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            MT[j][i] = M[i][j];
+        for(i=0;i<m2;i++){
+            for(j=0;j<n2;j++){
+                B[i][j] = rand()%10;
+            }
         }
-    }
-    mT = n;
-    nT = m;
 
-
-    printf("M\n");
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("%d\t",M[i][j]);
+        for(i=0;i<m1;i++){
+            for(j=0;j<n2;j++){
+                C[i][j] = A[i][j] + B[i][j];
+            }
         }
-        printf("\n");
-    }
-    printf("\n\n\n");
-
-    printf("MT\n");
-    for(i=0;i<mT;i++){
-        for(j=0;j<nT;j++){
-            printf("%d\t",MT[i][j]);
+        for(i=0;i<m1;i++){
+            for(j=0;j<n2;j++){
+                D[i][j] = A[i][j] - B[i][j];
+            }
         }
-        printf("\n");
+
+        printf("A\n");
+        for(i=0;i<m1;i++){
+            for(j=0;j<n1;j++){
+                printf("%d\t",A[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n\n\n");
+
+        printf("B\n");
+        for(i=0;i<m2;i++){
+            for(j=0;j<n2;j++){
+                printf("%d\t",B[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n\n\n");
+
+        printf("C\n");
+        for(i=0;i<m1;i++){
+            for(j=0;j<n2;j++){
+                printf("%d\t",C[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n\n\n");
+
+        printf("D\n");
+        for(i=0;i<m1;i++){
+            for(j=0;j<n2;j++){
+                printf("%d\t",D[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n\n\n");
+
     }
-    printf("\n\n\n");
+    else{
+        printf("Error! No pueden ni sumarse ni restarse... =^(\n");
+    }
 
     return 0;
 }
