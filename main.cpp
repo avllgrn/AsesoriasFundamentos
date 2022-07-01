4d6958d92898e35d0cba0287e71735afe4b47a2e
Alfredo Villagran Olguin <avillagran@ipn.mx>
2022-07-01 13:33:21 -0500

    Max Com Div, Min Com Mul (con vectores)

--- main.cpp
+++ main.cpp
@@ -1,31 +1,40 @@
 #include <stdio.h>
 
 int main(void){
-    int a,b,may,men,aux,mcm;
+    int a[100],b[100],i,aux,mcd,mcm,n;
 
-    printf("Dame a ");scanf("%d",&a);
-    printf("Dame b ");scanf("%d",&b);
+    printf("Dame a ");scanf("%d",&a[0]);
+    printf("Dame b ");scanf("%d",&b[0]);
 
-    if(a>b){
-        may = a;
-        men = b;
-    }
-    else{
-        may = b;
-        men = a;
-    }
-
-    while(may%men!=0){
-        aux=may%men;
-        may=men;
-        men=aux;
-        //printf("mcd = %d\n",men);
+    //Guardar el mayor de los dos, en a[0]
+    //Guardar el menor de los dos, en b[0]
+    if(a<b){
+        aux = a[0];
+        a[0] = b[0];
+        b[0] = aux;
     }
 
-    mcm = a*b/men;
+    //Algoritmo de Euclides
+    i=0;
+    while(a[i]%b[i]!=0){
+        a[i+1] = b[i];
+        b[i+1] = a[i]%b[i];
+        i++;
+    }
+    mcd = b[i];
+    mcm = a[0]*b[0]/mcd;
 
-    printf("\n\nmcd = %d",men);
-    printf("\n\nmcm = %d",mcm);
+    //Mostrar los vectores del algoritmo
+//    n=i;
+//    printf("\n\n");
+//    for(i=0;i<=n;i++){
+//        printf("%d\t%d\n",a[i],b[i]);
+//    }
+
+
+    printf("\n\nmcd = %d",mcd);
+    printf("\n\nmcm = %d\n\n",mcm);
+
 
     return 0;
 }
